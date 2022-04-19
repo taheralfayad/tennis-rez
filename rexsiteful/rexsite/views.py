3b580cee74d431226e2fab89a5a2f0cffd658611
Ahmad Altaher Alfayad <taheralfayad@gmail.com>
2022-04-18 21:42:27 -0400

    table + squashed some bugs

--- rexsiteful/rexsite/views.py
+++ rexsiteful/rexsite/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse
-from datetime import datetime
-import pdb 
+from datetime import datetime, date
+
 
 from urllib3 import HTTPResponse
 
@@ -16,11 +16,12 @@ def rexsite(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
     else:
-        return render(request, "rexsite/index.html")
+        return render(request, "rexsite/index.html", {
+            "Reservations": Reservation.objects.filter(dayof = date.today()).order_by("court", "starttime")
+        })
 
 
 def login_view(request):
-    breakpoint()
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -61,13 +62,15 @@ def reserve(request):
         end_time = datetime.strptime(request.POST["endtime"], '%H:%M')
         reservation = Reservation.objects.filter(court = court.id, dayof = dayof).values()
         for x in range(len(reservation)):
-            if (reservation[x]["starttime"].hour <= start_time.hour and reservation[x]["endtime"].hour >= end_time.hour):
+            if (reservation[x]["starttime"].hour <= start_time.hour and reservation[x]["endtime"].hour >= start_time.hour) or (reservation[x]["starttime"].hour <= end_time.hour and reservation[x]["endtime"].hour >= end_time.hour):
                 return render(request, "rexsite/index.html", {
-                    "message": "This court is already booked, please try another court or another time."
+                    "message": "This court is already booked, please try another court or another time.",
+                    "Reservations": Reservation.objects.filter(dayof = date.today()).order_by("court", "starttime")
                 })
         if end_time.hour > (start_time.hour + 2):
             return render(request, "rexsite/index.html", {
-                "message": "Your Reservation either exceeds 2 hours or is invalid, please input a vaild reservation"
+                "message": "Your Reservation either exceeds 2 hours or is invalid, please input a vaild reservation",
+                "Reservations": Reservation.objects.filter(dayof = date.today()).order_by("court", "starttime")
             })
         else:
             newrez = Reservation()
@@ -77,7 +80,8 @@ def reserve(request):
             newrez.endtime = request.POST["endtime"]
             newrez.save()
             return render(request, "rexsite/index.html", {
-                "message": "Your Reservation has been registered! Thanks for using Rec-Serve!"
+                "message": "Your Reservation has been registered! Thanks for using Rec-Serve!",
+                "Reservations": Reservation.objects.filter(dayof = date.today())
             })
 
 
